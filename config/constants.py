99d10ed1def7ae660bd5249e9f51df7db462f4cf
allenbenj <154886753+allenbenj@users.noreply.github.com>
2025-06-09 06:08:55 -0500

    Refactor config re-exports to avoid wildcard imports

--- config/constants.py
+++ config/constants.py
@@ -1,1 +1,33 @@
-from legal_ai_system.config.constants import *
+"""Public re-export of constants."""
+
+from legal_ai_system.config.constants import (
+    Constants,
+    EnvironmentConstants,
+    LegalConstants,
+    NetworkConstants,
+    PerformanceConstants,
+    SecurityConstants,
+    SizeConstants,
+    TimeConstants,
+    DocumentConstants,
+    validate_cache_size,
+    validate_float_range,
+    validate_positive_integer,
+    validate_timeout_seconds,
+)
+
+__all__ = [
+    "Constants",
+    "TimeConstants",
+    "SizeConstants",
+    "SecurityConstants",
+    "PerformanceConstants",
+    "DocumentConstants",
+    "LegalConstants",
+    "NetworkConstants",
+    "EnvironmentConstants",
+    "validate_positive_integer",
+    "validate_float_range",
+    "validate_timeout_seconds",
+    "validate_cache_size",
+]
