0142ba4c390c097069b5190442c6d9e4fd644389
allenbenj <154886753+allenbenj@users.noreply.github.com>
2025-06-10 05:57:50 -0500

    Add LegalWorkflowBuilder with parallel merge

--- legal_ai_system/workflows/__init__.py
+++ legal_ai_system/workflows/__init__.py
@@ -1,5 +1,14 @@
 """Workflow utilities and pipeline helpers."""
 
 from .agent_workflow import AgentWorkflow
+from .legal_workflow_builder import LegalWorkflowBuilder
+from .merge import MergeStrategy, FirstResultMerge, ListMerge, DictMerge
 
-__all__ = ["AgentWorkflow"]
+__all__ = [
+    "AgentWorkflow",
+    "LegalWorkflowBuilder",
+    "MergeStrategy",
+    "FirstResultMerge",
+    "ListMerge",
+    "DictMerge",
+]
