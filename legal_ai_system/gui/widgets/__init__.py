0ecbcf6d59d9ac52dc6ae290819bfb22e3be604d
allenbenj <154886753+allenbenj@users.noreply.github.com>
2025-06-12 04:54:12 -0500

    Add chart widgets with signal support

---
+++ legal_ai_system/gui/widgets/__init__.py
@@ -0,0 +1,14 @@
+"""Widget subpackage providing reusable PyQt6 components."""
+
+from .legal_ai_charts import (
+    AnalyticsDashboardWidget,
+    PieChartWidget,
+    BarChartWidget,
+)
+
+__all__ = [
+    "AnalyticsDashboardWidget",
+    "PieChartWidget",
+    "BarChartWidget",
+]
+
